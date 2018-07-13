c2e5377afc1cac14a5a1d4bf538bc9ff8a7779a7
ali aziz <aa_connect@outlook.com>
2018-07-13 13:38:00 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+    binding.pry
+  elsif int % 3 and 5 == 0
+    return "Fizzbuzz"
+  else
+    puts "Fizzbuzz"
+  end
+end
